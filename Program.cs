f2dd8c071cf74646725b99e6707dc20cbd369fcc
aminaHussien <aminamuhammed34286@gmail.com>
2026-07-21 14:50:48 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment2C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
