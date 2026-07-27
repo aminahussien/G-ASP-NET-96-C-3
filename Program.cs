a4d6761f4f4af45ba68fe918c3600600befd8fa0
aminaHussien <aminamuhammed34286@gmail.com>
2026-07-27 13:13:05 +0300

    Q2

--- Program.cs
+++ Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Q1
+            //#region
+            //double[] prices = { 25.5, 33.75, 40.0 };
+            //Console.WriteLine(prices[1]);
+            //#endregion
+
+            //Q2
+            #region
+            int[,] shelfCopies = {
+                {3,5},
+                {1,4}
+            };
+            Console.WriteLine(shelfCopies[1, 0]);
+            #endregion
         }
+
     }
 }
