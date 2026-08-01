5940468debd49195852442fc4c3295f5d5c49aa4
aminaHussien <aminamuhammed34286@gmail.com>
2026-08-01 14:25:42 +0300

    Q3

--- Program.cs
+++ Program.cs
@@ -4,25 +4,31 @@
     {
         private string password = "secret";
         internal int copiesInStock = 5;
-
+        public string title = "clean code";
     }
     public class Program
     {
         static void Main(string[] args)
         {
-            //Q1
+            ////Q1
+            //#region
+            //Book book = new Book();
+            //Console.WriteLine(book.password); //password is inaccessable 
+            //#endregion
+
+
+            ////Q2
+            //#region
+            //Book book = new Book();
+            //Console.WriteLine(book.copiesInStock); //copisInStock is accessable because it declared in class book as an internal field and internal acces modifiers can be accessed in the same project
+            //#endregion
+
+
+            //Q3
             #region
             Book book = new Book();
-            Console.WriteLine(book.password); //password is inaccessable 
+            Console.WriteLine(book.title);
             #endregion
-
-
-            //Q2
-            #region
-            Book book2 = new Book();
-            Console.WriteLine(book2.copiesInStock); //copisInStock is accessable because it declared in class book as an internal field and internal acces modifiers can be accessed in the same project
-            #endregion
-
 
         }
     }
