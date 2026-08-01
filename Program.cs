da82deaeebf7984d04807b813b20e706b489c2bf
aminaHussien <aminamuhammed34286@gmail.com>
2026-08-01 14:17:46 +0300

    Q2

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@
     public class Book
     {
         private string password = "secret";
+        internal int copiesInStock = 5;
 
     }
     public class Program
@@ -15,7 +16,14 @@
             Console.WriteLine(book.password); //password is inaccessable 
             #endregion
 
-           
+
+            //Q2
+            #region
+            Book book2 = new Book();
+            Console.WriteLine(book2.copiesInStock); //copisInStock is accessable because it declared in class book as an internal field and internal acces modifiers can be accessed in the same project
+            #endregion
+
+
         }
     }
 }
