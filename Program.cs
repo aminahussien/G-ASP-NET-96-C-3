baf0f483fbb63c28261fbeed4256e71401ee136c
aminaHussien <aminamuhammed34286@gmail.com>
2026-08-25 18:58:57 +0300

    Q1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
-﻿namespace Assignment2C_
+﻿using Assignment2C_.Structs;
+namespace Assignment2C_
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Q1
+            DeliveryAddress delivery = new DeliveryAddress("alex","roushdy",5);
+            DeliveryAddress delivery2 = new DeliveryAddress("cairo", "maadi", 7);
+            Console.WriteLine( delivery.GetFullAddress());
+            Console.WriteLine(delivery2.GetFullAddress());
+            delivery2 = delivery;
+            Console.WriteLine(delivery.GetFullAddress());
+            Console.WriteLine(delivery2.GetFullAddress());
+
+
         }
     }
 }
---
+++ Structs/DeliveryAddress.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Assignment2C_.Structs
+{
+    internal struct DeliveryAddress
+    {
+        string city;
+        string street;
+        int buildingNumber;
+
+        public DeliveryAddress(string city, string street, int buildingNumber)
+        {
+            this.city = city;
+            this.street = street;
+            this.buildingNumber = buildingNumber;
+        }
+        public string GetFullAddress()
+        {
+            return $"{this.city} --{this.street}-- {this.buildingNumber}";
+        }
+    }
+}
